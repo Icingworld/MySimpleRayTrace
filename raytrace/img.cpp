85f5e19f049e4c0d118de729c709e338a2e2dfa2
icingworld <984442022@qq.com>
2023-08-10 16:48:14 +0800

    complete bmp operator

--- raytrace/img.cpp
+++ raytrace/img.cpp
@@ -1,1 +1,61 @@
 #include "img.hpp"
+
+void Image::setFile(const std::string & path)
+{
+    PATH = path;
+}
+
+void Image::setSize(const int width, const int height)
+{
+    SIZE[0] = width;
+    SIZE[1] = height;
+    initImage();
+}
+
+void Image::initImage()
+{
+    // 初始化图像2维数组
+    Array array(SIZE[0], SIZE[1]);
+    IMAGE = array.array();
+}
+
+void Image::readImage(int *** data)
+{
+    for (int i = 0; i < SIZE[0]; i++) {
+        for (int j = 0; j < SIZE[1]; j++) {
+            IMAGE[i][j] = data[i][j];
+        }
+    }
+}
+
+void Image::initHead()
+{
+    HEAD.signature = 0x4D42; // "BM"
+    HEAD.fileSize = sizeof(BMPHeader) + SIZE[0] * SIZE[1] * 3; // 3 bytes per pixel for 24-bit color
+    HEAD.reserved1 = 0;
+    HEAD.reserved2 = 0;
+    HEAD.dataOffset = sizeof(BMPHeader);
+    HEAD.headerSize = 40;
+    HEAD.width = SIZE[0];
+    HEAD.height = SIZE[1];
+    HEAD.planes = 1;
+    HEAD.bitsPerPixel = 24;
+    HEAD.compression = 0;
+    HEAD.dataSize = SIZE[0] * SIZE[1] * 3;
+    HEAD.horizontalResolution = 2835; // 72 dpi
+    HEAD.verticalResolution = 2835; // 72 dpi
+    HEAD.colors = 0;
+    HEAD.importantColors = 0;
+}
+
+void Image::writeBmp()
+{
+    std::ofstream bmpFile(PATH, std::ios::binary);
+    if (!bmpFile) {
+        std::cout << "Error opening file for writing!" << std::endl;
+        return;
+    }
+    bmpFile.write(reinterpret_cast<char*>(&HEAD), sizeof(BMPHeader));
+    bmpFile.write(reinterpret_cast<char*>(IMAGE), SIZE[0] * SIZE[1] * 3);
+    bmpFile.close();
+}
--- raytrace/img.hpp
+++ raytrace/img.hpp
@@ -1,6 +1,49 @@
 #ifndef _IMG_HPP_
 #define _IMG_HPP_
 
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include "array.hpp"
 
+class Image
+{
+private:
+    std::string PATH{};
+    int SIZE[2];
+    int *** IMAGE;
+    struct BMPHeader {
+        uint16_t    signature;
+        uint32_t    fileSize;
+        uint16_t    reserved1;
+        uint16_t    reserved2;
+        uint32_t    dataOffset;
+        uint32_t    headerSize;
+        int32_t     width;
+        int32_t     height;
+        uint16_t    planes;
+        uint16_t    bitsPerPixel;
+        uint32_t    compression;
+        uint32_t    dataSize;
+        int32_t     horizontalResolution;
+        int32_t     verticalResolution;
+        uint32_t    colors;
+        uint32_t    importantColors;
+    } HEAD;
+public:
+    Image() {};
+    Image(const int size[2]) : SIZE{size[0], size[1]} { initImage(); initHead(); };
+
+    // 设置信息
+    void setFile(const std::string &);
+    void setSize(const int, const int);
+    void initImage();
+    void initHead();
+    void readImage(int ***);
+
+    // 写入图像
+    void writeBmp();
+};
 
 #endif
