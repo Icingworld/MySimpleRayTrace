f183f56cc61a8db0d15cf5bcc12e04264962f067
icingworld <984442022@qq.com>
2023-08-10 17:36:30 +0800

    modify BMP HEAD

--- raytrace/img.hpp
+++ raytrace/img.hpp
@@ -7,6 +7,46 @@
 #include <cstdint>
 #include "array.hpp"
 
+// 位图文件头
+typedef struct tagBITMAPFILEHEADER {
+	uint16_t bfType;			//文件类型
+	uint32_t bfSize;			//文件大小
+	uint16_t bfReserved1;		//保留字
+	uint16_t bfReserved2;		//保留字
+	uint32_t bfOffBits;		    //文件头到实际图像数据之间的偏移量
+}BITMAPFILEHEADER;
+
+// 位图信息头
+typedef struct tagBITMAPINFOHEADER {
+	DWORD biSize;			    //位图信息头大小
+	LONG biWidth;			    //位图宽度
+	LONG biHeight;			    //位图高度
+	WORD biPlanes;			    //颜色平面数
+	WORD biBitCount;		    //每个像素的位数
+	DWORD biCompression;	    //压缩方式
+	DWORD biSizeImage;	    	//位图全部像素占用的字节数
+	LONG biXPelsPerMeter;   	//水平方向分辨率
+	LONG biYPelsPerMeter;   	//垂直方向分辨率
+	DWORD biClrUsed;		    //使用的颜色数
+	DWORD biClrImportant;   	//重要颜色数
+}BITMAPINFOHEADER;
+
+// 调色板
+typedef struct tagRGBQUAD {
+	BYTE rgbBlue;
+	BYTE rgbGreen;
+	BYTE rgbRed;
+	BYTE rgbReserved;
+}RGBQUAD;
+
+// 位图数据
+typedef struct tagIMAGEDATA {
+	BYTE red;
+	BYTE green;
+	BYTE blue;
+}IMAGEDATA;
+
+
 class Image
 {
 private:
