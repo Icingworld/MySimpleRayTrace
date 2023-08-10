25f0f07136bd5205b325058150f8cb3f3583d5a6
icingworld <984442022@qq.com>
2023-08-10 16:50:15 +0800

    modify bmp test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include "vec3.hpp"
+#include "ray.hpp"
 #include "img.hpp"
+#include "array.hpp"
 #include <iostream>
 
 int main()
 {
-    vec3 point(1, 2, 3);
-    std::cout << point.x() << point.y() << point.z() << std::endl;
-    std::cout << point.modulo() << std::endl;
-    point *= 3;
-    std::cout << point.x() << point.y() << point.z() << std::endl;
-    std::cout << point.modulo() << std::endl;
-    -point;
-    std::cout << point.x() << point.y() << point.z() << std::endl;
-    std::cout << point.modulo() << std::endl;
+    int size[2]{40, 30};
+    Image image(size);
+    color test(213, 166, 189);
+    Array img(size[0], size[1]);
+    for (int i = 0; i < size[0]; i++) {
+        for (int j = 0; j < size[1]; j++) {
+            std::clog << "Max: " << "i: " << size[0] << " j: " << size[1] << "\n" << std::flush;
+            std::clog << "ScanPixel: " << i << "   " << j << "\n" << std::flush;
+            // 这里内存有问题
+            img[i][j] = test.toIntVec();
+        }
+    }
+    std::clog << "Get All Pixel !\n" << std::flush;
+    image.setFile("test.bmp");
+    image.readImage(img.array());
+    image.writeBmp();
+    std::clog << "Done !\n" << std::flush;
     return 0;
 }
