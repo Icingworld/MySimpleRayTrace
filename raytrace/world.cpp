b00b09777660fd54143991ac262d5f3f2b497d25
icingworld <984442022@qq.com>
2023-08-11 17:17:09 +0800

    add class world

---
+++ raytrace/world.cpp
@@ -0,0 +1,51 @@
+#include "world.hpp"
+
+void World::setLight(const point & newLight)
+{
+    LIGHT.modify(newLight);
+}
+
+void World::setCamera(const int width, const int heigth, const int interval)
+{
+    WIDTH = width;
+    HEIGHT = heigth;
+    INTERVAL = interval;
+}
+
+void World::addObject(const Object & newObject)
+{
+    OBJECTS.push_back(newObject);
+}
+
+void World::setDepth(const int depth)
+{
+    MAX = depth;
+}
+
+void World::render()
+{
+    point raw;
+    for (int i = 0; i < WIDTH; i++) {
+        for (int j = 0; j < HEIGHT; j++) {
+            // 计算视口上该点坐标
+            // 这里有笛卡尔坐标系问题,需查询bmp格式
+            point viewPoint = {getCoordinate(i, WIDTH), getCoordinate(j, HEIGHT), INTERVAL};
+            // 获得射线
+            Ray ray(raw, viewPoint/viewPoint.modulo());
+            // 计算和球体的交点
+            // TODO: 先完成Object类
+        }
+    }
+}
+
+double World::getCoordinate(const int i, const int all) const
+{
+    if (static_cast<int>((static_cast<double>(all) / 2.0)) * 2 == all)
+    {
+        // 是偶数
+        return static_cast<double>(i) + 0.5 - static_cast<double>(all) / 2.0;
+    } else {
+        // 是奇数
+        return static_cast<double>(i) - (static_cast<double>(all - 1)) / 2.0;
+    }
+}
---
+++ raytrace/world.hpp
@@ -0,0 +1,36 @@
+#ifndef _WORLD_HPP_
+#define _WORLD_HPP_
+
+#include <vector>
+#include "vec3.hpp"
+#include "object.hpp"
+#include "ray.hpp"
+
+class World
+{
+private:
+    // 右手坐标系世界
+    point LIGHT;
+    // 视口大小,间隔大小
+    int WIDTH{400}, HEIGHT{300}, INTERVAL{100};
+    // 存放世界中的物体
+    std::vector <Object> OBJECTS;
+    // 迭代深度
+    int MAX{3};
+public:
+    World() : LIGHT{0, 400, 300} {};
+    // 修改光源位置0
+    void setLight(const point &);
+    // 设置相机和视口
+    void setCamera(const int, const int, const int);
+    // 添加物体，目前仅支持球体
+    void addObject(const Object &);
+    // 修改深度
+    void setDepth(const int);
+    // 渲染光线追踪
+    void render();
+    // 计算视口点坐标
+    double getCoordinate(const int, const int) const;
+};
+
+#endif
