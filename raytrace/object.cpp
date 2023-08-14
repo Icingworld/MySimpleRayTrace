98e8a6b236e1f21a52218c1b8a00e10ef6f4a2a9
icingworld <984442022@qq.com>
2023-08-14 09:09:47 +0800

    complete class object

--- raytrace/object.cpp
+++ raytrace/object.cpp
@@ -1,1 +1,16 @@
 #include "object.hpp"
+
+point Object::center() const
+{
+    return CENTER;
+}
+
+double Object::radius() const
+{
+    return RADIUS;
+}
+
+int Object::material() const
+{
+    return MATERIAL;
+}
--- raytrace/object.hpp
+++ raytrace/object.hpp
@@ -1,9 +1,21 @@
 #ifndef _OBJECT_HPP_
 #define _OBJECT_HPP_
 
+#include "vec3.hpp"
+
+enum Material { GLASS, ROUGH };
+
 class Object
 {
-
+private:
+    point CENTER;
+    double RADIUS;
+    int MATERIAL;
+public:
+    Object(point & center, double radius, int material) : CENTER(center), RADIUS(radius), MATERIAL(material) {};
+    point center() const;
+    double radius() const;
+    int material() const;
 };
 
 #endif
